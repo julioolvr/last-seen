d530a13cab63a176a1a7dbce55172623efdcea47
Julio Olivera <julio.olvr@gmail.com>
2017-12-20 20:33:08 -0500

    Initial commit

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Quick server to get the token from Foursquare's Oauth flow
+// Flow instructions at
+// https://developer.foursquare.com/docs/api/configuration/authentication
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Got code: %s", r.URL.Query()["code"][0])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
